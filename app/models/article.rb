1501852d56697394041835ccde73791dc4bd3abd
berke <bugdayberke@gmail.com>
2020-10-27 21:19:50 +0300

    like button

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :likes, dependent: :destroy
   belongs_to :user
   validates :title, presence: true,
             length: { minimum: 3 }
