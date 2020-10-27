7cbe0be8f85d6586545134d512602573776e2696
berke <bugdayberke@gmail.com>
2020-10-27 13:40:25 +0300

    articles

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true,
+            length: { minimum: 3 }
+  validates :text, presence: true,
+            length: { minimum: 5}
+  #not sure if it's needed
+  validates :user, presence:true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_154858) do
+ActiveRecord::Schema.define(version: 2020_10_25_180830) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
